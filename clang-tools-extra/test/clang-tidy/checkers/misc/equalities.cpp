b7eeb468f04e30dc71cca370ad8a0e314025b692
Brice Decaestecker <brice.decaestecker@gmx.fr>
2025-04-30 07:58:28 +0200

    Setup e2e test

--- clang-tools-extra/test/clang-tidy/checkers/misc/equalities.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/misc/equalities.cpp
@@ -4,53 +4,50 @@ struct S {
     int i;
     int j;
 
+
+    // Body-related tests
+
     [[clang::annotate("deriving_eq")]]
-    bool f_undefined(const S& s) const;
-    
-    /// @brief 
-    /// @param s 
-    [[clang::annotate("deriving_eq")]]
-    void f_void(const S& s) const {}
-    // CHECK-MESSAGES: :[[@LINE-1]]:10: warning: function 'f_void' has empty body but should return a boolean value [misc-equalities]
-    // CHECK-MESSAGES: :[[@LINE-2]]:10: warning: function 'f_void' has non boolean return type [misc-equalities]
-    // CHECK-MESSAGES: :[[@LINE-3]]:10: warning: function 'f_void' signature is not suitable for an equality operator [misc-equalities]
-    // CHECK-MESSAGES: :[[@LINE-4]]:35: warning: function 'f_void' should consist of a single return statement composed of binary equality comparisons [misc-equalities]
-    // CHECK-FIXES: { return (i == s.i) && (j == s.j); }
+    bool f_void(const S& s) const {}
+    // CHECK-MESSAGES: :[[@LINE-1]]:35: warning: function 'f_void' has empty body but should return a boolean value
+    // CHECK-MESSAGES: :[[@LINE-2]]:35: warning: function 'f_void' should consist of a single return statement composed of binary equality comparisons [misc-equalities]
+    // CHECK-FIXES: bool f_void(const S& s) const  { return (i == s.i) && (j == s.j); }
+
     [[clang::annotate("deriving_eq")]]
     bool f_too_many_statements(const S& s) const {
-        bool r = true;
-        return r;
+        bool eq_i = i == s.i;
+        bool eq_j = j == s.j;
+        return eq_i && eq_j;
     }
+    // CHECK-MESSAGES: :[[@LINE-5]]:50: warning: function 'f_too_many_statements' should consist of a single return statement [misc-equalities]
+    // CHECK-MESSAGES: :[[@LINE-6]]:50: warning: function 'f_too_many_statements' should consist of a single return statement composed of binary equality comparisons [misc-equalities]
+    // CHECK-FIXES: bool f_too_many_statements(const S& s) const  { return (i == s.i) && (j == s.j); }
+    
+    // Signature-related tests
+    [[clang::annotate("deriving_eq")]]
+    bool f_undefined_ok(const S& s) const;
+    
+    [[clang::annotate("deriving_eq")]]
+    bool f_non_const_param(S& s) const;
+    // CHECK-MESSAGES: :[[@LINE-1]]:10: warning: function 'f_non_const_param' signature is not suitable for an equality operator [misc-equalities]
+    // CHECK-MESSAGES: :[[@LINE-2]]:31: warning: parameter 's' should be const qualified [misc-equalities]
+    // CHECK-FIXES: bool f_non_const_param(S const& s) const;
+    
+    [[clang::annotate("deriving_eq")]]
+    bool f_non_const(const S& s);
+    // CHECK-MESSAGES: :[[@LINE-1]]:10: warning: function 'f_non_const' should be const [misc-equalities]
+    // CHECK-MESSAGES: :[[@LINE-2]]:10: warning: function 'f_non_const' signature is not suitable for an equality operator [misc-equalities]
+    // CHECK-FIXES: bool f_non_const(S const& s) const;
+    
+    [[clang::annotate("deriving_eq")]]
+    bool f_non_ref(const S s) const;
+    // CHECK-MESSAGES: :[[@LINE-1]]:10: warning: function 'f_non_ref' signature is not suitable for an equality operator [misc-equalities]
+    // CHECK-MESSAGES: :[[@LINE-2]]:28: warning: parameter 's' should be passed by reference [misc-equalities]
+    // CHECK-FIXES: bool f_non_ref(S const& s) const;
 
     [[clang::annotate("deriving_eq")]]
-    bool f_no_return(const S& s) const {
-        bool r = true;
-    }
-
-    [[clang::annotate("deriving_eq")]]
-    bool f_no_single_param() const {
-        bool r = true;
-    }
-
-    [[clang::annotate("deriving_eq")]]
-    bool f_non_const_param(S &s) const {
-        return true;
-    }
-
-    [[clang::annotate("deriving_eq")]]
-    bool f_non_ref_param(const S s) const {
-        return true;
-    }
-
-    [[clang::annotate("deriving_eq")]]
-    bool f_non_const(const S& s) {
-        return true;
-    }
-
-    [[clang::annotate("deriving_eq")]]
-    bool f_ok(const S& s) const {
-        return (true == true) && (false == false);
-    }
+    bool f_wrong_param_type(const int& s) const;
+    // CHECK-MESSAGES: :[[@LINE-1]]:10: warning: function 'f_wrong_param_type' has invalid argument type 'int' for equality with 'S' [misc-equalities]
+    // CHECK-MESSAGES: :[[@LINE-2]]:10: warning: function 'f_wrong_param_type' signature is not suitable for an equality operator [misc-equalities]
+    // CHECK-FIXES: bool f_wrong_param_type(S const& s) const;
 };
-
-void awesome_f2();
