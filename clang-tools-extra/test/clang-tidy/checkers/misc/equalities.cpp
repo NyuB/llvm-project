6c74c8be3fdbf363beb83b8b2602c836c7f475e9
Brice Decaestecker <brice.decaestecker@gmx.fr>
2025-05-02 07:24:56 +0200

    Continue tests

--- clang-tools-extra/test/clang-tidy/checkers/misc/equalities.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/misc/equalities.cpp
@@ -22,8 +22,14 @@ struct S {
     // CHECK-MESSAGES: :[[@LINE-5]]:50: warning: function 'f_too_many_statements' should consist of a single return statement [misc-equalities]
     // CHECK-MESSAGES: :[[@LINE-6]]:50: warning: function 'f_too_many_statements' should consist of a single return statement composed of binary equality comparisons [misc-equalities]
     // CHECK-FIXES: bool f_too_many_statements(const S& s) const  { return (i == s.i) && (j == s.j); }
+
+    [[clang::annotate("deriving_eq")]]
+    bool f_ok(const S&s) const {
+        return i == s.i && j == s.j;
+    }
     
     // Signature-related tests
+
     [[clang::annotate("deriving_eq")]]
     bool f_undefined_ok(const S& s) const;
     
@@ -50,4 +56,15 @@ struct S {
     // CHECK-MESSAGES: :[[@LINE-1]]:10: warning: function 'f_wrong_param_type' has invalid argument type 'int' for equality with 'S' [misc-equalities]
     // CHECK-MESSAGES: :[[@LINE-2]]:10: warning: function 'f_wrong_param_type' signature is not suitable for an equality operator [misc-equalities]
     // CHECK-FIXES: bool f_wrong_param_type(S const& s) const;
+
+    [[clang::annotate("deriving_eq")]]
+    bool f_defined_later(const S& s) const;
 };
+
+
+bool S::f_defined_later(const S& s) const {
+    return true;
+}
+// CHECK-MESSAGES: :[[@LINE-3]]:43: warning: function 'f_defined_later' should consist of a single return statement composed of binary equality comparisons [misc-equalities]
+// CHECK-MESSAGES: :[[@LINE-3]]:12: warning: function 'f_defined_later' returned expression should be a boolean conjonction of equalities [misc-equalities]
+// CHECK-FIXES: bool S::f_defined_later(const S& s) const  { return (i == s.i) && (j == s.j); }
